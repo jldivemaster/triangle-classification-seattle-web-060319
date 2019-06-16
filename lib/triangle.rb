4fc26555bea1ee4c12d3e8a867687ca521552570
Jason Leach <jlspursfan@gmail.com>
2019-06-16 22:48:53 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,7 +8,14 @@ class Triangle
   end
 
   def kind
-
+    if (side1 <= 0) || (side2 <= 0) || (side3 <= 0) || (side1 + side2 <= side3) || (side2 + side3 <= side1) || (side1 + side3 <= side2)
+      raise TriangleError
+    elsif
+      :equilateral
+    elsif
+      :isosceles
+    elsif
+      :scalene
   end
 
   class TriangleError < StandardError
