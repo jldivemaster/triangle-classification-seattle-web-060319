1f4f4fc0df6623bfc4bc533af96f7c8285d54be9
Jason Leach <jlspursfan@gmail.com>
2019-06-16 23:07:02 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -7,7 +7,7 @@ class Triangle
     @side3 = side3
   end
 
-  def kind(triangle)
+  def kind
     if (@side1 <= 0) || (@side2 <= 0) || (@side3 <= 0) || (@side1 + @side2 <= @side3) || (@side2 + @side3 <= @side1) || (@side1 + @side3 <= @side2)
       begin
         raise TriangleError
